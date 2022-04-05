715b71a0be51443c11dd51aef1c027bf75f4b2d0
TiCefops <SistemasCefops@2022>
2022-04-05 16:03:47 -0300

    implementação de testes

---
+++ test/mocks/user_mock.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+
+class UserMock{
+
+  final user = MockUser(
+    isAnonymous: false,
+    uid: '1a2b3c4b5b6b7b78b',
+    email: 'teste1@gmail.com',
+    displayName: 'Teste Login',
+
+
+  );
+}
