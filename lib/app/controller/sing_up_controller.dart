dc10f12b0f49a50da852b86f25bc1cb6bb5821fa
TiCefops <SistemasCefops@2022>
2022-04-29 09:06:57 -0300

    implementado verificação cpf já cadastrado

--- lib/app/controller/sing_up_controller.dart
+++ lib/app/controller/sing_up_controller.dart
@@ -1,5 +1,7 @@
+import 'package:easmaterialdidatico/app/data/interface/user_data_info_interface.dart';
 import 'package:easmaterialdidatico/app/data/repository/course_repository.dart';
 import 'package:easmaterialdidatico/app/data/repository/singup_repository.dart';
+import 'package:easmaterialdidatico/app/data/repository/user_data_info_repository.dart';
 import 'package:easmaterialdidatico/app/erroHandler/auth_handdler.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easmaterialdidatico/shared/themes/app_colors.dart';
@@ -10,9 +12,12 @@ import 'package:get/get.dart';
 import '../../shared/auth/firebase_auth.dart';
 import '../routes/app_routes.dart';
 
-class SingupController extends GetxController {
+
+
+class SingUpController extends GetxController {
   final SingUpRepository repository = SingUpRepository();
   final CourseRepository _courseRepository = CourseRepository();
+  final IUserDataInfo _userDataInfo=UserDataInfoRepository();
   final courseId = Get.parameters;
 
   @override
@@ -23,23 +28,23 @@ class SingupController extends GetxController {
   }
 
   RxBool loadingPage = false.obs;
-  RxBool showPassord = false.obs;
+  RxBool showPassword = false.obs;
   RxString password = "".obs;
   RxString name = "".obs;
   RxString cpf = "".obs;
   RxString email = "".obs;
-  RxString errorMessagerForSnack = "".obs;
+  RxString errorMessengerForSnack = "".obs;
   RxString courseName = "Carregando...".obs;
-  RxString courseIdforSingup = "".obs;
-  RxString moduleIdforSingup = "".obs;
+  RxString courseIdForSingUp = "".obs;
+  RxString moduleIdForSingUp = "".obs;
   RxBool buttonEnabled = false.obs;
 
   Future<void> getAndSetCourseInfoById(String id) async {
     DocumentSnapshot snapshot = await _courseRepository.getCourseInfoById(id);
     if (snapshot.data() != null) {
       courseName.value = snapshot.get("nome");
-      courseIdforSingup.value = snapshot.get("id");
-      moduleIdforSingup.value = snapshot.get("firstModule");
+      courseIdForSingUp.value = snapshot.get("id");
+      moduleIdForSingUp.value = snapshot.get("firstModule");
       buttonEnabled.value=snapshot.get("receberCadastro");
       if(snapshot.get("receberCadastro")==false){
        Get.showSnackbar(const GetSnackBar(
@@ -56,40 +61,43 @@ class SingupController extends GetxController {
     }
   }
 
-  setErrorMenssagerForSnack(String error) {
-    errorMessagerForSnack.value = AuthHanddler().errorFilter(error);
+  setErrorMessengerForSnack(String error) {
+    errorMessengerForSnack.value = AuthHanddler().errorFilter(error);
   }
 
-  Future<void> singUp({required String email,required String passowrd}) async {
+  Future<void> singUp({required String email,required String password}) async {
+    loadingPage.value=true;
     String cpf0= cpf.value.replaceAll(".", "");
-    String cpf1=cpf0.removeAllWhitespace.replaceAll("-", "");
-      loadingPage.value=true;
-    await AuthenticationHelper(auth: FirebaseAuth.instance)
+    String cpfFormatted=cpf0.removeAllWhitespace.replaceAll("-", "");
+    bool userExist=await _userDataInfo.checkUserExist(cpfFormatted);
+
+    userExist? Get.snackbar("Erro", "CPF já Cadastrado",colorText: Colors.red):await AuthenticationHelper(auth: FirebaseAuth.instance)
         .signUp(
-        email: email, password: passowrd)
+        email: email, password: password)
         .then((result) async {
       if (result is UserCredential) {
         await AuthenticationHelper(auth: FirebaseAuth.instance)
             .signIn(
             email: email,
-            password: passowrd)
+            password: password)
             .then(
-              (value) async {
-                User? user = FirebaseAuth.instance.currentUser;
-                String uid = user!.uid;
-                await repository.singUp(name.value,cpf1, email, uid,
-                  courseIdforSingup.value, moduleIdforSingup.value);}
+                (value) async {
+              User? user = FirebaseAuth.instance.currentUser;
+              String uid = user!.uid;
+              await repository.singUp(name.value,cpfFormatted, email, uid,
+                  courseIdForSingUp.value, moduleIdForSingUp.value);}
         );
         loadingPage.value = false;
         Get.offAndToNamed(Routes.HOME);
       } else {
         loadingPage.value = false;
-        setErrorMenssagerForSnack(result);
+        setErrorMessengerForSnack(result);
         Get.snackbar(
             "Falha no Cadastro",
-            errorMessagerForSnack.value);
+            errorMessengerForSnack.value);
       }
     });
+    loadingPage.value = false;
 
 
 
