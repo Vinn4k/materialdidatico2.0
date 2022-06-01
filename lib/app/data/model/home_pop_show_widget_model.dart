f62af9513d2ab69aea43bbbe046e7415cfb2d082
TiCefops <SistemasCefops@2022>
2022-06-01 16:06:24 -0300

    adicionado camo tituloPop e tituloBotao

--- lib/app/data/model/home_pop_show_widget_model.dart
+++ lib/app/data/model/home_pop_show_widget_model.dart
@@ -1,27 +1,47 @@
 class HomePopShowWidgetModel {
   HomePopShowWidgetModel({
+      this.tituloPop, 
       this.curso, 
       this.mostrar, 
       this.imgUrl, 
-      this.paginaMatricula,});
+      this.paginaMatricula, 
+      this.tituloBotao,});
 
   HomePopShowWidgetModel.fromJson(dynamic json) {
+    tituloPop = json['tituloPop'];
     curso = json['curso'];
     mostrar = json['mostrar'];
     imgUrl = json['imgUrl'];
     paginaMatricula = json['paginaMatricula'];
+    tituloBotao = json['tituloBotao'];
   }
+  String? tituloPop;
   String? curso;
   bool? mostrar;
   String? imgUrl;
   String? paginaMatricula;
-
+  String? tituloBotao;
+HomePopShowWidgetModel copyWith({  String? tituloPop,
+  String? curso,
+  bool? mostrar,
+  String? imgUrl,
+  String? paginaMatricula,
+  String? tituloBotao,
+}) => HomePopShowWidgetModel(  tituloPop: tituloPop ?? this.tituloPop,
+  curso: curso ?? this.curso,
+  mostrar: mostrar ?? this.mostrar,
+  imgUrl: imgUrl ?? this.imgUrl,
+  paginaMatricula: paginaMatricula ?? this.paginaMatricula,
+  tituloBotao: tituloBotao ?? this.tituloBotao,
+);
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
+    map['tituloPop'] = tituloPop;
     map['curso'] = curso;
     map['mostrar'] = mostrar;
     map['imgUrl'] = imgUrl;
     map['paginaMatricula'] = paginaMatricula;
+    map['tituloBotao'] = tituloBotao;
     return map;
   }
 
