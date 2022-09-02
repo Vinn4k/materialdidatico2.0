8925e4d9592af06787691a9842828a5aa297f572
TiCefops <SistemasCefops@2022>
2022-09-02 12:32:16 -0300

    sistema de encriptar e desencriptar o pdf

---
+++ lib/app/security/crypt_pdf_service.dart
@@ -0,0 +1,22 @@
+import 'package:encrypt/encrypt.dart' as crypto;
+
+class CryptPdfService{
+
+
+  Future<String> encryptPdf({required String pdfBase64}) async {
+    final key = crypto.Key.fromUtf8("3gA2dkfTtlkNBdXgWcVQnhRkkWC5dxWc");
+    final iv = crypto.IV.fromLength(16);
+    final encrypterData = crypto.Encrypter(crypto.AES(key));
+    final pdfEncrypted =  encrypterData.encrypt(pdfBase64, iv: iv);
+    return pdfEncrypted.base64.toString();
+
+  }
+
+  Future<String> decryptPdf({required String pdfEncrypted}) async {
+    final key = crypto.Key.fromUtf8("3gA2dkfTtlkNBdXgWcVQnhRkkWC5dxWc");
+    final iv = crypto.IV.fromLength(16);
+    final decryptLocalPdf = crypto.Encrypter(crypto.AES(key));
+    final pdfDecrypted = decryptLocalPdf.decrypt64(pdfEncrypted, iv: iv);
+    return pdfDecrypted;
+  }
+}
