b585a3a26b8019d96d3dc99f3d3f990cff095b86
TiCefops <SistemasCefops@2022>
2022-09-02 12:32:42 -0300

    adicionado encriptar arquivo no download

--- lib/app/services/file_service.dart
+++ lib/app/services/file_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:easmaterialdidatico/app/security/crypt_pdf_service.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -12,18 +13,20 @@ class FileService {
     final targetDir = await getExternalStorageDirectory();
     final targetDirFolder = Directory('${targetDir?.path}/off');
     final path = targetDirFolder.path;
-    final File file = File('$path/$fileName.pdf');
+    final File file = File('$path/$fileName.eas');
     targetDirFolder.existsSync() ? null : targetDirFolder.create();
     final status = await Permission.storage.request();
 
     final Dio dio = Dio();
     try {
       if (status.isGranted) {
-        await dio.download(url, file.path, deleteOnError: true,
-            onReceiveProgress: (rec, total) {
-
-        }).then((value) =>
-            saveDataOffPdf(filePath: file.path, documentId: fileName));
+        await dio
+            .download(url, file.path,
+                deleteOnError: true, onReceiveProgress: (rec, total) {})
+            .then((value) async {
+          await encryptPdf(pdfPath: file.path);
+          await saveDataOffPdf(filePath: file.path, documentId: fileName);
+        });
       } else {
         Get.snackbar(
             "Atenção ", "Permissão para Acessar o armazenamento não concedida");
@@ -31,6 +34,26 @@ class FileService {
     } catch (e) {
       Get.snackbar("Error ", "Não foi possível Concluir o Download");
     }
+  }
+
+  Future<void> encryptPdf({required String pdfPath}) async {
+    final CryptPdfService pdfService = CryptPdfService();
+    final localPdf = await File(pdfPath).readAsBytes();
+    final localPdfToBase64 = base64.encode(localPdf);
+
+    final encryptedPdf =
+        await pdfService.encryptPdf(pdfBase64: localPdfToBase64);
+    final file = File(pdfPath);
+    await file.writeAsString(encryptedPdf);
+    await decryptPdf(pdfPath: file.path);
+  }
+
+  Future<String> decryptPdf({required String pdfPath}) async {
+    final CryptPdfService pdfService = CryptPdfService();
+
+    final file = File(pdfPath);
+    final encryptedPdf = await file.readAsString();
+    return await pdfService.decryptPdf(pdfEncrypted: encryptedPdf);
   }
 
   Future<bool> pdfIsSync({required String id}) async {
@@ -42,22 +65,17 @@ class FileService {
     } else {
       ispresent = false;
     }
-    //
-    // final targetDir = await getExternalStorageDirectory();
-    // final  targetDirFolder = Directory('${targetDir?.path}/off');
-    // final path=targetDirFolder.path;
-    // final File file=File('$path/$id.pdf');
     return ispresent;
   }
 
-  Future<File> getOffPdf({required String id}) async {
+  Future<String> getOffPdf({required String id}) async {
     final prefs = await SharedPreferences.getInstance();
     String? localData = prefs.getString(id);
 
     Map<String, dynamic> dataModel = json.decode(localData!);
     String path = dataModel["filePath"];
-    final File file = File(path);
-    return file;
+    final String pdfDecrypted = await decryptPdf(pdfPath: path);
+    return pdfDecrypted;
   }
 
   Future<void> saveDataOffPdf(
@@ -70,6 +88,5 @@ class FileService {
     };
     String data = json.encode(dataModel);
     prefs.setString(documentId, data);
-    print(prefs.get(documentId));
   }
 }
