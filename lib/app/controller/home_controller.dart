187f0038f3941adc6ce4de06ca0730e497f75fd6
TiCefops <SistemasCefops@2022>
2022-09-02 13:56:27 -0300

    nd

--- lib/app/controller/home_controller.dart
+++ lib/app/controller/home_controller.dart
@@ -10,9 +10,7 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:ota_update/ota_update.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:device_info_plus/device_info_plus.dart';
+
 import '../../shared/auth/firebase_auth.dart';
 import '../../shared/themes/app_colors.dart';
 import '../data/model/home_pop_show_widget_model.dart';
@@ -27,22 +25,17 @@ import '../widgets/home_pop_show_widget.dart';
 class HomeController extends GetxController {
   final IDataItens _repository = DataItensRepository();
   final IUserDataInfo _dataUserInfo = UserDataInfoRepository();
-  late PackageInfo packageInfo;
 
   final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
   final FirebaseRemoteConfig firebaseRmConfig = FirebaseRemoteConfig.instance;
   final AppMonitorRepository _monitorRepository = AppMonitorRepository();
 
-  String appVersionLocal = "";
 
   @override
   Future<void> onInit() async {
-    packageInfo = await PackageInfo.fromPlatform();
-    appVersionLocal = packageInfo.version;
     await userCheck();
     super.onInit();
-    versionCheck();
-   
+
   }
 
   @override
@@ -61,11 +54,7 @@ class HomeController extends GetxController {
 
   RxList userCourseId = [].obs;
   RxBool showAdds = true.obs;
-  RxString appVerisonSever = "1.1.3".obs;
-  RxBool updateApp = false.obs;
-  RxDouble downloadProgress = 0.0.obs;
-  RxString linkApk="".obs;
-  RxString ssh="".obs;
+
 
   Future<String> getCourseID() async {
     User? user = FirebaseAuth.instance.currentUser;
@@ -163,116 +152,6 @@ Future<void> userGroupVerify()async{
 
 }
   
-  ///mostra popup para atualizar a versão do app
-  Future<void> versionCheck() async {
-    _monitorRepository.getApInfo().then(
-      (value) {
-        appVerisonSever.value = value.get("versao");
 
-        if (appVersionLocal != appVerisonSever.value) {
-          set32or64BitApk(value);
-          if (!GetPlatform.isWeb) {
-            showAdds.value=false;
-            
-            Get.defaultDialog(
-                title: "Atualização Encontrada",
-                buttonColor: AppColors.blue,
-                cancelTextColor: Colors.black,
-                confirmTextColor: Colors.white,
-                content: SizedBox(child: StreamBuilder(
-                    stream: downloadProgress.stream,
-                    builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
-                      double value=snapshot.data??00;
-                      return Column(
-                        children: [
-                          LinearProgressIndicator(
-                            value:value/100,
-                            backgroundColor: AppColors.orange,
-                            color: AppColors.blue,
-                          ),
-                          Text("$value %")
-                        ],
-                      );
-                    })),
-                onCancel: () {
-                  showAdds.value=true;
-                },
-                onConfirm: () async {
-                  await otaUpdate().then((value) =>    showAdds.value=true);
-
-
-                },
-                textConfirm: "Atualizar",
-                textCancel: "Cancelar");
-          }
-        }
-      },
-    );
-  }
-Future<void> set32or64BitApk(DocumentSnapshot data)async{
-   final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-   Map deviceData = <String, dynamic>{};
-  deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
-
-List arm32=deviceData["supported32BitAbis"];
-List arm64=deviceData["supported64BitAbis"];
-
-  if(arm32.isNotEmpty){
-    linkApk.value=data.get("urlApk32");
-    ssh.value=data.get("sshApk32");
-  }if(arm64.isNotEmpty){
-     linkApk.value=data.get("urlApk64");
-     ssh.value=data.get("sshApk64");
-   }
-
-}
-  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
-    return <String, dynamic>{
-      'version.securityPatch': build.version.securityPatch,
-      'version.sdkInt': build.version.sdkInt,
-      'version.release': build.version.release,
-      'version.previewSdkInt': build.version.previewSdkInt,
-      'version.incremental': build.version.incremental,
-      'version.codename': build.version.codename,
-      'version.baseOS': build.version.baseOS,
-      'board': build.board,
-      'bootloader': build.bootloader,
-      'brand': build.brand,
-      'device': build.device,
-      'display': build.display,
-      'fingerprint': build.fingerprint,
-      'hardware': build.hardware,
-      'host': build.host,
-      'id': build.id,
-      'manufacturer': build.manufacturer,
-      'model': build.model,
-      'product': build.product,
-      'supported32BitAbis': build.supported32BitAbis,
-      'supported64BitAbis': build.supported64BitAbis,
-      'supportedAbis': build.supportedAbis,
-      'tags': build.tags,
-      'type': build.type,
-      'isPhysicalDevice': build.isPhysicalDevice,
-      'androidId': build.androidId,
-      'systemFeatures': build.systemFeatures,
-    };
-  }
-  Future<void> otaUpdate() async {
-
-    try {
-      OtaUpdate()
-          .execute(
-        linkApk.value,
-        destinationFilename: 'easmaterial.apk',
-      )
-          .listen(
-        (OtaEvent event) {
-          downloadProgress.value = double.parse(event.value ?? "");
-        },
-      );
-    } catch (e) {
-      throw ("Falha ao atualizar");
-    }
-  }
 
 }
