af20a3ed317396338d75b9f8fddd8824fcec8209
TiCefops <SistemasCefops@2022>
2022-09-05 16:36:21 -0300

    removido função de performa

--- lib/app/controller/pdf_view_controller.dart
+++ lib/app/controller/pdf_view_controller.dart
@@ -1,6 +1,4 @@
 
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,44 +11,38 @@ import '../data/repository/user_data_info_repository.dart';
 import '../routes/app_routes.dart';
 import '../services/file_service.dart';
 
-
-
-class PdfViewerControllerUi extends GetxController{
+class PdfViewerControllerUi extends GetxController {
   final UserDataInfoRepository _repository = UserDataInfoRepository();
- final User? user=FirebaseAuth.instance.currentUser;
-  final FileService _service=FileService();
+  final User? user = FirebaseAuth.instance.currentUser;
+  final FileService _service = FileService();
   final data = Get.parameters;
   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
-
   @override
-  void onInit()async {
-    await pdfIsSync();
+  void onInit() async {
     if (user == null) {
-        Get.offAndToNamed(Routes.LOGIN);
+      Get.offAndToNamed(Routes.LOGIN);
     }
-   await getUserInfo();
+    await getUserInfo();
     await setDevice();
 
     await analytics.setUserId(id: user!.uid);
     super.onInit();
   }
 
-
-  RxString userCpf="".obs;
+  RxString userCpf = "".obs;
   RxString userName = "".obs;
   RxString userID = "".obs;
-  RxString userDiviceType="".obs;
-  RxString password="O2!iGi%IL6H6Ob0yByjK".obs;
-  RxBool pdfIsSynced=false.obs;
-  RxString offPdfBase64="".obs;
-
+  RxString userDiviceType = "".obs;
+  RxString password = "O2!iGi%IL6H6Ob0yByjK".obs;
+  RxBool pdfIsSynced = false.obs;
+  RxString offPdfBase64 = "".obs;
+  RxBool pdfDownloadComplete = true.obs;
 
   Future<DocumentSnapshot> getUserInfo() async {
     User? user = FirebaseAuth.instance.currentUser;
-    String uid=user?.uid??"";
-    DocumentSnapshot snapshot =
-    await _repository.getUserInfo(uid);
+    String uid = user?.uid ?? "";
+    DocumentSnapshot snapshot = await _repository.getUserInfo(uid);
     var data = snapshot.data();
     UserInfoModel userInfoModel = UserInfoModel.fromJson(data);
     userName.value = userInfoModel.nome!;
@@ -59,85 +51,87 @@ class PdfViewerControllerUi extends GetxController{
 
     return snapshot;
   }
-  void cpfFormatterForPage(String cpf){
-    String cpf0=cpf .substring(0,3);
-    String cpf2=cpf .substring(3,6);
-    String cpf3=cpf .substring(6,9);
-    String cpf4=cpf .substring(9,11);
-    userCpf.value="$cpf0.$cpf2.$cpf3-$cpf4";
 
-
+  void cpfFormatterForPage(String cpf) {
+    String cpf0 = cpf.substring(0, 3);
+    String cpf2 = cpf.substring(3, 6);
+    String cpf3 = cpf.substring(6, 9);
+    String cpf4 = cpf.substring(9, 11);
+    userCpf.value = "$cpf0.$cpf2.$cpf3-$cpf4";
   }
 
-  double setZoomInPc(){
-  if(GetPlatform.isMobile){
-    return 0.0;
-  }else{
-    return 1.25;
+  double setZoomInPc() {
+    if (GetPlatform.isMobile) {
+      return 0.0;
+    } else {
+      return 1.25;
+    }
   }
 
-  }
-  Future<void> automaticDownload()async{
-    if(GetPlatform.isAndroid){
-      String offlinePath=data["path"]??"not";
-      if(offlinePath=="not"){
-        String pdf = data["linkPdf"]??"error";
+  Future<void> automaticDownload() async {
+    if (GetPlatform.isAndroid) {
+      String offlinePath = data["path"] ?? "not";
+      if (offlinePath == "not") {
+        String pdf = data["linkPdf"] ?? "error";
         String id = data["id"] ?? "error";
         downloadPdf(url: pdf, fileName: id);
       }
     }
   }
-Future<void> downloadPdf({required String url,required String fileName})async{
-  await _service.downloadPdf(url:url,fileName: fileName);
-}
 
-Future<bool> pdfIsSync()async{
-    String id=data['id']??"ERROR";
-    final isOffline= await _service.pdfIsSync(id: id);
-    pdfIsSynced.value=isOffline;
-    if(isOffline){
-      String base64Pdf=await _service.getOffPdf(id: id);
-      offPdfBase64.value=base64Pdf;
+  Future<void> downloadPdf(
+      {required String url, required String fileName}) async {
+    pdfDownloadComplete.value = false;
+    await _service
+        .downloadPdf(url: url, fileName: fileName)
+        .then((value) => pdfDownloadComplete.value = true)
+        .onError((error, stackTrace) => pdfDownloadComplete.value = true);
+  }
 
+  String pdfDec(String path) {
+    return _service.decryptPdf(pdfPath: path);
+  }
+
+  Future<String> pdfIsSync() async {
+    String id = data['id'] ?? "ERROR";
+    final isOffline = await _service.pdfIsSync(id: id);
+    if (isOffline != "nd") {
+      offPdfBase64.value = isOffline;
     }
 
     return isOffline;
-}
-  Future<void> startTrace(Trace trace) async {
-    await trace.start();
   }
+
+
+
   Future<void> stopTrace(Trace trace) async {
     await trace.stop();
   }
-
 
   Future<void> disableScreenCapture() async {
     //disable screenshots and record screen in current screen
     await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
   }
- setDevice(){
 
-   if  (GetPlatform.isLinux) {
-     userDiviceType.value = "Linux";
-   } else {
-     if (GetPlatform.isAndroid) {
-       userDiviceType.value = "Android";
-     } else if (GetPlatform.isIOS) {
-       userDiviceType.value = "IOS";
-     } else if (GetPlatform.isFuchsia) {
-       userDiviceType.value = "Fuchsia";
-     } else if(GetPlatform.isMobile) {
-       userDiviceType.value = "Mobile";
-     } else if (GetPlatform.isMacOS) {
-       userDiviceType.value = "MacOS";
-     } else if (GetPlatform.isWindows) {
-       userDiviceType.value = "Windows";
-     }else if (GetPlatform.isWeb) {
-       userDiviceType.value = "Web";
-     }
-   }
-
- }
-
-
+  setDevice() {
+    if (GetPlatform.isLinux) {
+      userDiviceType.value = "Linux";
+    } else {
+      if (GetPlatform.isAndroid) {
+        userDiviceType.value = "Android";
+      } else if (GetPlatform.isIOS) {
+        userDiviceType.value = "IOS";
+      } else if (GetPlatform.isFuchsia) {
+        userDiviceType.value = "Fuchsia";
+      } else if (GetPlatform.isMobile) {
+        userDiviceType.value = "Mobile";
+      } else if (GetPlatform.isMacOS) {
+        userDiviceType.value = "MacOS";
+      } else if (GetPlatform.isWindows) {
+        userDiviceType.value = "Windows";
+      } else if (GetPlatform.isWeb) {
+        userDiviceType.value = "Web";
+      }
+    }
+  }
 }
